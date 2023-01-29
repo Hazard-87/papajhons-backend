ee4b2d07a1d2c2e776346a3f183e91ee9fd604ac
neo <dpetryshkin@gmail.com>
2023-01-29 12:41:13 +0400

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('/api')
   app.enableCors({
-    origin: ['http://localhost:3000/', 'https://www.google.com'],
+    origin: true,
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
