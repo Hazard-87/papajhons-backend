9fc30d53faeeecff441cb08dee1b9eddf664ee63
neo <dpetryshkin@gmail.com>
2023-01-29 12:43:10 +0400

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import { ValidationPipe } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('/api')
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.enableCors({
-    origin: true,
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
+    allowedHeaders: "*",
+    origin: "*"
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   await app.listen(8080)
 }
