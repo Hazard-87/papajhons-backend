32a3c57e0aac39987f7d6a44ef5ab3c4bb335383
neo <dpetryshkin@gmail.com>
2023-02-09 01:06:33 +0400

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { resolve } from 'path'
+import { writeFileSync } from 'fs'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -15,9 +17,19 @@ async function bootstrap() {
 
   const config = new DocumentBuilder().build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('/swagger', app, document)
 
   await app.listen(8080)
+
+  if (process.env.NODE_ENV === 'development') {
+    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static')
+
+    // write swagger json file
+    const pathToSwaggerJson = resolve(pathToSwaggerStaticFolder, 'swagger.json')
+    const swaggerJson = JSON.stringify(document, null, 2)
+    writeFileSync(pathToSwaggerJson, swaggerJson)
+    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`)
+  }
 }
 
 bootstrap()
