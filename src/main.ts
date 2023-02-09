4504bd6b946602d65f512f34ae166d04a60d9366
neo <dpetryshkin@gmail.com>
2023-02-09 10:53:02 +0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-import { createWriteStream } from 'fs'
-import { get } from 'http'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
